45cbe2b435025ea5c1df8abad99f7f521d90ed2d
Maftunbek <prince777_98@mail.ru>
2019-09-10 20:36:15 +0500

    Add library for knockout.js #5 Add UI form with two input field #3

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,12 +1,27 @@
 package controllers
 
+import com.typesafe.scalalogging.LazyLogging
 import javax.inject._
 import play.api.mvc._
+import views.html._
+
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+class HomeController @Inject()(val controllerComponents: ControllerComponents,
+                               indexTemplate: index,
+                               subjectTemplate: subject,
+                              )
+                              (implicit val ec: ExecutionContext)
+  extends BaseController with LazyLogging {
+
+  def index: Action[AnyContent] = Action {
+    Ok(indexTemplate())
   }
+
+  def subject: Action[AnyContent] = Action {
+    Ok(subjectTemplate())
+  }
+
 
 }
