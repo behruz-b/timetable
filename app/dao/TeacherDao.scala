8752a8a0ef92060bbaf4ee60c703b54d421ecf0d
Maftunbek <prince777_98@mail.ru>
2019-09-12 15:24:10 +0500

    TeacherDao is created And table structure is created on default.sql #19

---
+++ app/dao/TeacherDao.scala
@@ -0,0 +1,54 @@
+package dao
+
+import com.google.inject.ImplementedBy
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject.{Inject, Singleton}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
+import utils.Date2SqlDate
+
+
+import scala.concurrent.Future
+
+
+trait SubjectComponent { self: HasDatabaseConfigProvider[JdbcProfile] =>
+
+  import utils.PostgresDriver.api._
+
+  class TeachersTable(tag: Tag) extends Table[Teachers](tag, "Teachers") with Date2SqlDate  {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+    def firstName = column[String]("firstName")
+
+    def lastName = column[String]("lastName")
+
+    def tSubject = column[String]("tSubject")
+
+    def department = column[String]("department")
+
+    def * = (id.?, firstName, lastName, tSubject, department) <> (Teacher.tupled, Teacher.unapply _)
+  }
+}
+
+@ImplementedBy(classOf[TeacherDaoImpl])
+trait TeacherDao {
+  def addTeacher(teacherData: Teacher): Future[Int]
+}
+
+@Singleton
+class TeacherDaoImpl @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
+  extends TeacherDao
+    with TeacherComponent
+    with HasDatabaseConfigProvider[JdbcProfile]
+    with LazyLogging {
+
+  import utils.PostgresDriver.api._
+
+  val teachers = TableQuery[TeachersTable]
+
+  override def addTeacher(teacherData: Teacher): Future[Int] = {
+    db.run {
+      (teachers returning teachers.map(_.id)) += teacherData
+    }
+  }
+}
--- conf/evolutions/default/default.sql
+++ conf/evolutions/default/default.sql
@@ -1,9 +1,19 @@
 # --- !Ups
+firstName, lastName, tSubject, department
 CREATE TABLE "Subject" (
   "id" SERIAL NOT NULL PRIMARY KEY,
   "name" VARCHAR NOT NULL,
   "numberClassRoom" INTEGER NOT NULL
 );
 
+CREATE TABLE "Teachers" (
+  "id" SERIAL NOT NULL PRIMARY KEY,
+  "firstName" VARCHAR NOT NULL,
+  "lastName" VARCHAR NOT NULL,
+  "tSubject" VARCHAR NOT NULL,
+  "department" VARCHAR NOT NULL
+);
+
 # --- !Downs
 DROP TABLE "Subject";
+DROP TABLE "Teachers";
