69aeac8cd6e4d598b7e935c71d4cba1213566beb
Maftunbek <prince777_98@mail.ru>
2019-09-16 17:34:40 +0500

    GroupProtocol, GroupManager and addGroup in GroupDao is created #28

---
+++ app/actors/GroupManager.scala
@@ -0,0 +1,36 @@
+package actors
+
+import akka.actor.{Actor, ActorLogging}
+import akka.pattern.pipe
+import akka.util.Timeout
+import dao.GroupDao
+import javax.inject.Inject
+import play.api.Environment
+import protocols.GroupProtocol.{AddGroup, Group}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+
+class GroupManager @Inject()(val environment: Environment,
+                             groupDao: GroupDao
+                            )
+                            (implicit val ec: ExecutionContext)
+  extends Actor with ActorLogging {
+
+  implicit val defaultTimeout: Timeout = Timeout(60.seconds)
+
+  def receive = {
+    case AddGroup(group) =>
+      addGroup(group).pipeTo(sender())
+
+    case _ => log.info(s"received unknown message")
+
+  }
+
+  private def addGroup(groupData: Group) = {
+    groupDao.addGroup(groupData).map { data =>
+      data
+    }
+  }
+
+}
