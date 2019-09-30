c4f4f459e942bcd3bbc19894af69eedd55762af9
Behruz <behruz.ravshonbekovich@gmail.com>
2019-09-30 09:36:43 +0500

    import optimizing

--- app/telegrambot/MyAmazingBot.java
+++ app/telegrambot/MyAmazingBot.java
@@ -5,8 +5,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.logging.Logger;
-
 public class MyAmazingBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
