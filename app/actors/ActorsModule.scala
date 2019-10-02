a2d0dcb6598e1766e8cead0dc7474ce6465e1979
Behruz <behruz.ravshonbekovich@gmail.com>
2019-10-02 11:27:00 +0500

    Removed telegram service from project

--- app/actors/ActorsModule.scala
+++ app/actors/ActorsModule.scala
@@ -9,6 +9,5 @@ class ActorsModule extends AbstractModule with AkkaGuiceSupport {
     bindActor[TeacherManager]("teacher-manager")
     bindActor[TimetableManager]("timetable-manager")
     bindActor[GroupManager]("group-manager")
-    bindActor[TelegramManager]("telegram-manager")
   }
 }
