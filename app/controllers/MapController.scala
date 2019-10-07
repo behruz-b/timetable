0d6f62e4500de9a92fac3b637837dd8d2b0d57f4
Maftunbek <prince777_98@mail.ru>
2019-10-07 15:57:37 +0500

    map floor1 is added to project

---
+++ app/controllers/MapController.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.util.Timeout
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject._
+import play.api.libs.json.Json
+import play.api.mvc._
+import protocols.GroupProtocol._
+import views.html._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+
+@Singleton
+class MapController @Inject()(val controllerComponents: ControllerComponents,
+                              @Named("group-manager") val groupManager: ActorRef,
+                              floor1Template: map.floor1,
+                                )
+                             (implicit val ec: ExecutionContext)
+  extends BaseController with LazyLogging {
+
+  implicit val defaultTimeout: Timeout = Timeout(60.seconds)
+
+  def floor1: Action[AnyContent] = Action {
+      Ok(floor1Template())
+  }
+
+}
