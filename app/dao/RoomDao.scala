9232485729bebf17806b8c1f1a62a225e9a03ab9
Maftunbek <prince777_98@mail.ru>
2020-01-17 14:40:45 +0500

    Updater room is added

--- app/dao/RoomDao.scala
+++ app/dao/RoomDao.scala
@@ -19,11 +19,11 @@ trait RoomComponent {
   class RoomTable(tag: Tag) extends Table[Room](tag, "Room") with Date2SqlDate {
     def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
 
-    def number = column[String]("name")
+    def number = column[String]("number")
 
-    def place = column[Int]("direction")
+    def place = column[Int]("place")
 
-    def * = (id.?, number, place) <> (Room.tupled, Room.unapply _)
+    def * = (id.?, number, place.?) <> (Room.tupled, Room.unapply _)
   }
 
 }
@@ -31,6 +31,10 @@ trait RoomComponent {
 @ImplementedBy(classOf[RoomDaoImpl])
 trait RoomDao {
   def addRoom(data: Room): Future[Int]
+
+  def getRoomList: Future[Seq[Room]]
+
+  def update(data: Room): Future[Int]
 
 }
 
@@ -48,7 +52,20 @@ class RoomDaoImpl @Inject()(protected val dbConfigProvider: DatabaseConfigProvid
 
   override def addRoom(data: Room): Future[Int] = {
     db.run {
+      logger.warn(s"room: $data")
       (rooms returning rooms.map(_.id)) += data
+    }
+  }
+
+  override def getRoomList: Future[Seq[Room]] = {
+    db.run {
+      rooms.result
+    }
+  }
+
+  override def update(data: Room): Future[Int] = {
+    db.run {
+      rooms.filter(_.id === data.id).update(data)
     }
   }
 }
